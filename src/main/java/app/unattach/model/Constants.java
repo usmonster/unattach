d57c3c4e986be4b7d6b226cb7cd2db845bcbafa1
Rok Strniša <rok@strnisa.com>
2020-12-20 15:48:30 +0000

    v3.0.0

--- src/main/java/app/unattach/model/Constants.java
+++ src/main/java/app/unattach/model/Constants.java
@@ -12,5 +12,5 @@ public final class Constants {
   public static final String DONATE_URL = HOMEPAGE + "/?action=buy_coffee";
   public static final String TUTORIAL_VIDEO_URL = "https://unattach.app/tutorial-video";
   public static final String PRODUCT_NAME = "Unattach";
-  public static final String VERSION = "2.4.0";
+  public static final String VERSION = "3.0.0";
 }
