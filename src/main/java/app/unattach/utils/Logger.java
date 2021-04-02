4f1de3e28cf17ccd12fcd31c1ff205d4a266dd68
Rok Strniša <rok@strnisa.com>
2021-04-02 12:18:36 +0100

    Switch to log4j2.

--- src/main/java/app/unattach/utils/Logger.java
+++ src/main/java/app/unattach/utils/Logger.java
@@ -1,13 +1,6 @@
 package app.unattach.utils;
 
-import static org.apache.log4j.Level.*;
-
-public record Logger(org.apache.log4j.Logger logger) {
-  /**
-   * Passing this to the super calls ensures that the log lines are correct.
-   */
-  private static final String FQCN = Logger.class.getName();
-
+public record Logger(org.apache.logging.log4j.Logger logger) {
   /**
    * @return A logger with the name of the class that called this method.
    */
@@ -18,42 +11,38 @@ public record Logger(org.apache.log4j.Logger logger) {
   }
 
   public static Logger get(String name) {
-    return new Logger(org.apache.log4j.Logger.getLogger(name));
-  }
-
-  static Logger getRoot() {
-    return new Logger(org.apache.log4j.Logger.getRootLogger());
+    return new Logger(org.apache.logging.log4j.LogManager.getLogger(name));
   }
 
   public void debug(Object message) {
-    logger.log(FQCN, DEBUG, message, null);
+    logger.debug(message);
   }
 
   public void debug(String format, Object... args) {
-    logger.log(FQCN, DEBUG, String.format(format, args), null);
+    logger.debug(String.format(format, args));
   }
 
   public void info(Object message) {
-    logger.log(FQCN, INFO, message, null);
+    logger.info(message);
   }
 
   public void info(String format, Object... args) {
-    logger.log(FQCN, INFO, String.format(format, args), null);
+    logger.info(String.format(format, args));
   }
 
   public void warn(String format, Object... args) {
-    logger.log(FQCN, WARN, String.format(format, args), null);
+    logger.warn(String.format(format, args));
   }
 
   public void error(Object message) {
-    logger.log(FQCN, ERROR, message, null);
+    logger.error(message);
   }
 
   public void error(String format, Object... args) {
-    logger.log(FQCN, ERROR, String.format(format, args), null);
+    logger.error(String.format(format, args));
   }
 
   public void error(Object message, Throwable t) {
-    logger.log(FQCN, ERROR, message, t);
+    logger.error(message, t);
   }
 }
