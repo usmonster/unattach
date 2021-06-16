3148c26f50308c9074770635d8d94e1cfcdd63e0
Rok Strniša <rok@strnisa.com>
2021-06-16 20:45:44 +0100

    Upgrade Process column and remove Status column.

--- src/main/java/app/unattach/model/EmailStatus.java
+++ src/main/java/app/unattach/model/EmailStatus.java
@@ -1,8 +1,15 @@
 package app.unattach.model;
 
+import org.apache.commons.lang3.StringUtils;
+
 public enum EmailStatus {
   FAILED,
   NOT_SELECTED,
   PROCESSED,
-  TO_PROCESS
+  TO_PROCESS;
+
+  @Override
+  public String toString() {
+    return StringUtils.capitalize(name().toLowerCase().replace('_', ' '));
+  }
 }
--- src/main/java/app/unattach/view/SelectedCheckBoxTableCellFactory.java
+++ src/main/java/app/unattach/view/SelectedCheckBoxTableCellFactory.java
@@ -8,7 +8,9 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Tooltip;
 import javafx.util.Callback;
+import org.apache.commons.lang3.StringUtils;
 
 public class SelectedCheckBoxTableCellFactory
     implements Callback<TableColumn.CellDataFeatures<Email, CheckBox>, ObservableValue<CheckBox>> {
@@ -17,10 +19,14 @@ public class SelectedCheckBoxTableCellFactory
     Email email = cellDataFeatures.getValue();
     CheckBox checkBox = new CheckBox();
     TableView<Email> tableView = cellDataFeatures.getTableView();
-    boolean enabled = tableView.isEditable() && email.getStatus() != EmailStatus.FAILED &&
-        email.getStatus() != EmailStatus.PROCESSED;
-    checkBox.setDisable(!enabled);
     checkBox.selectedProperty().setValue(email.isSelected());
+    checkBox.getStyleClass().removeAll();
+    if (email.getStatus() == EmailStatus.PROCESSED) {
+      checkBox.getStyleClass().add("checkbox-processed");
+    } else if (email.getStatus() == EmailStatus.FAILED) {
+      checkBox.getStyleClass().add("checkbox-failed");
+    }
+    checkBox.tooltipProperty().setValue(new Tooltip(email.getStatus().toString()));
     checkBox.selectedProperty().addListener((observableValue, oldValue, newValue) -> {
       EmailStatus targetStatus = newValue ? EmailStatus.TO_PROCESS : EmailStatus.NOT_SELECTED;
       ObservableList<Email> selectedEmails = tableView.getSelectionModel().getSelectedItems();
